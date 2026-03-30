df16e04b32aa63640b6d6363ad44c32e1babfa59
Guilherme De Rosa <guilhermeperesderosa@gmail.com>
2026-03-30 09:15:38 -0300

    feat: Declarações e variaveis

--- src/script.js
+++ src/script.js
@@ -1,1 +1,45 @@
-alert('Hello, World!')
+//Declarações e variáveis
+//var, let e const
+
+var nome="Fiap";
+console.log(nome);
+var nome="Teste";
+console.log(nome);
+
+if(true){
+    let apelido="Fiapin";
+    console.log(apelido);   
+}
+
+const taxaJuros=0.05;
+console.log(taxaJuros);
+var exemplo="ola-dev-var";
+console.log(exemplo);
+
+//typeof verifica o tipo da variavel
+// let exemplo1="ola-dev-let";
+// console.log(exemplo1);
+
+let exemplo1="ola-dev-let";
+console.log(exemplo1);
+
+const exemplo2="ola-dev-const";
+console.log(exemplo2);
+
+let exemplo3=10+1;
+console.log(exemplo3);
+
+let exemplo4=true;
+console.log(exemplo4);
+
+let exemplo5={nome1:"Teste"};
+console.log(exemplo5);
+
+let exemplo6=["Java", "C#", "PHP"];
+console.log(exemplo6);
+
+//valor undefined 
+let exemplo7;
+console.log(exemplo7);
+let exemplo8= null;
+console.log(exemplo8);
